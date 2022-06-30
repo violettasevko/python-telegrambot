ebdebf7d1523e4c86201c3b32ded18415730b79c
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2022-07-01 01:53:05 +0300

    commit

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == "Hi":
+        bot.send_message(message.chat.id, f"Hi, {message.from_user.first_name}", parse_mode='html')
+    elif message.text == "id":
+        bot.send_message(message.chat.id, f"Your id is {message.from_user.id}", parse_mode='html')
+    else:
+        bot.send_message(message.chat.id, "Invalid input", parse_mode='html')
+
+
 bot.polling(none_stop=True)
